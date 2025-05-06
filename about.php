d9b396eefe20fed8d4531ff671b8b38fbc6a5350
JoeChindemi <joechindemi@gmail.com>
2025-05-06 16:04:46 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: About Page
+ */
+
+get_header(); ?>
+
+<main class="site-main container">
+    <h1>About Us</h1>
+    <p>This is a test About Page by Joe</p>
+</main>
+
+<?php get_footer(); ?>
